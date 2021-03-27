140c60eef8b84c3bf3ed30a6255737ef7260b913
wangechimk <wangechik.michelle@gmail.com>
2021-03-27 20:32:47 +0300

    add quote-display component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuoteDisplayComponent } from './quote-display/quote-display.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuoteFormComponent
+    QuoteFormComponent,
+    QuoteDisplayComponent
   ],
   imports: [
     BrowserModule,
