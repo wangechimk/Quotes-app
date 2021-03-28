fe372c8b0ffb5898a7192753706f8efe1fb41350
wangechimk <wangechik.michelle@gmail.com>
2021-03-29 00:00:13 +0300

    add changes to the quotes form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,22 +11,41 @@ export class AppComponent {
   quotes: Quote[] = [
     {
       id: 1,
-      date: new Date('12-03-2019'),
-      upvotes: 0,
+      date: new Date('09-01-2021'),
+      upvotes: 13,
       downvotes: 2,
-      name: 'Jake Sully',
-      quote: 'Sometimes your whole life boils down to one insane move.',
-      author: 'James Cameron',
+      name: 'Alex',
+      quote: 'There is no limit to what we, as women, can accomplish',
+      author: 'Michelle Obama',
     },
     {
       id: 2,
       date: new Date('10-04-2010'),
-      upvotes: 0,
-      downvotes: 2,
+      upvotes: 8,
+      downvotes: 0,
       name: 'paul',
       quote: 'Of course I am not worried about intimidating men. The type of man who will be intimidated by me is exactly the type of man I have no interest in.',
       author: 'Chimamanda Ngozi Adichie',
-    }
+    },
+    {
+      id: 3,
+      date: new Date('15-09-2014'),
+      upvotes: 10,
+      downvotes: 3,
+      name: 'Amy',
+      quote: 'Women, if the soul of the nation is to be saved, I believe you must become its soul.',
+      author: 'Coretta Scott King',
+    },
+    {
+      id: 4,
+      date: new Date('15-09-2014'),
+      upvotes: 20,
+      downvotes: 4,
+      name: 'Beryl',
+      quote: '"Women will be hidden no more. We will not remain hidden figures. We have names. … It was woman that gave you Dr. Martin Luther King, Jr. It was woman that gave you Malcolm X. And according to the Bible, it was a woman that gave you Jesus. Dont you ever forget it.',
+      author: 'Janelle Monáe',
+    },
+    
   ];
 
   getNewQuote(e) {
